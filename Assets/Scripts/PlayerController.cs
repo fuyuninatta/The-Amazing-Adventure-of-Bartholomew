4a6f31bcda27ce16333d2b65cec616ccd04b2662
yohan102 <yohanyohanhanhan@gmail.com>
2026-07-15 22:59:03 +0800

    set up all gun, set up load next scene
    Done:
    1. press 1,2,3 to change gun
    2. use "maxGunIndex" to unlock new weapon level by level
    3. save the ammo data through levels
    4. use tag "NextLevel" to load new scene.
    
    Need improve:
    1. change particle effects of all weapon.
    2. dont forget to set "maxGunIndex" to 1 in 1st level.
    3. dont forget to set the level sequences.
    4. havent add sfx for all weapon.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,11 @@
 using NUnit.Framework.Interfaces;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
-    public float moveSpeed,runSpeed,gravityModifier,jumpingPower;
+    public float moveSpeed, runSpeed, gravityModifier, jumpingPower;
     public CharacterController charCon;
     private Vector3 moveInput;
     public Transform camTrans;
@@ -13,8 +14,12 @@ public class PlayerController : MonoBehaviour
 
     public float mouseSensitivity;
 
-    public GameObject bullet;
-    public Transform firepoint;
+    //public GameObject bullet;
+    public Transform firePoint;
+
+    public Gun activeGun;
+    public List<Gun> allGuns = new List<Gun>();
+    public int currentGun, maxGunIndex = 1;
 
     private void Awake()
     {
@@ -23,7 +28,28 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+        activeGun = allGuns[currentGun];
+        activeGun.gameObject.SetActive(true);
+
+        for (int i = 0; i < allGuns.Count; i++)
+        {
+            allGuns[i].PreparePool();
+
+            //get maxGunIndex
+            if(PlayerPrefs.HasKey("maxGunIndex"))
+            {
+                maxGunIndex = PlayerPrefs.GetInt("maxGunIndex");
+            }
+            
+
+            //get current ammo
+            if(PlayerPrefs.HasKey("Gun_"+i+"_Ammo"))//prevent error
+            {
+                allGuns[i].currentAmmo = PlayerPrefs.GetInt("Gun_" + i + "_Ammo");
+            }
+        }
+        
+        UIController.instance.ammoText.text = "AMMO: " + activeGun.currentAmmo;
     }
 
     // Update is called once per frame
@@ -41,22 +67,23 @@ public class PlayerController : MonoBehaviour
         moveInput = vertMove + horiMove;
         moveInput.Normalize();
 
-        if(Input.GetKey(KeyCode.LeftShift))
+
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             //running speed
             moveInput *= runSpeed;
         }
         else
-        {   
+        {
             //walking speed;
             moveInput *= moveSpeed;
         }
-        
+
         moveInput.y = yStore;
 
         moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
 
-        if(charCon.isGrounded)
+        if (charCon.isGrounded)
         {
             jumping = 2;
             moveInput.y = -1f;
@@ -69,30 +96,101 @@ public class PlayerController : MonoBehaviour
             jumping--;
         }
 
-        
+
 
         charCon.Move(moveInput * Time.deltaTime);
-        float movemag = new Vector3(charCon.velocity.x,0,charCon.velocity.z).magnitude;
+        float movemag = new Vector3(charCon.velocity.x, 0, charCon.velocity.z).magnitude;
         anim.SetFloat("MoveSpeed", movemag);
 
         //player looking rotation(left and right)
-        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"))*mouseSensitivity;
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
-        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y,0f,0f));
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
 
-        if (Input.GetMouseButtonDown(0))
+        //Handle the shooting
+        if (Input.GetMouseButtonDown(0) && activeGun.fireCounter <= 0)
         {
-            RaycastHit hit;
-            if(Physics.Raycast(camTrans.position,camTrans.forward, out hit,50f))
-            {
-                firepoint.LookAt(hit.point);
-            }
-            else
-            {
-                firepoint.LookAt(camTrans.position + (camTrans.forward * 30f));
-            }
-               
-            Instantiate(bullet, firepoint.position, firepoint.rotation);
+            fireShot();
+        }
+
+        if (Input.GetMouseButton(0) && activeGun.canAutoFire && activeGun.fireCounter <= 0)
+        {
+            fireShot();
+        }
+
+        //Switch gun base on what player press (1-3)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchGun(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwitchGun(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SwitchGun(2);
+        }
+    }
+    public void fireShot()
+    {
+        if (activeGun.currentAmmo <= 0)
+        {
+            return;
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(camTrans.position, camTrans.forward, out hit, 50f))
+        {
+            firePoint.LookAt(hit.point);
+        }
+        else
+        {
+            firePoint.LookAt(camTrans.position + camTrans.forward * 30f);
+        }
+
+        activeGun.currentAmmo--;
+
+        activeGun.GetBullet(firePoint.position, firePoint.rotation);
+
+        activeGun.fireCounter = activeGun.fireRate;
+
+        UIController.instance.ammoText.text = "AMMO: " + activeGun.currentAmmo;
+    }
+
+    public void SwitchGun(int currentGunIndex)
+    {
+        activeGun.gameObject.SetActive(false);
+
+        //switch gun when the gun is unlocked
+        //maxGunIndex will +1 when player enter new level(each level unlock 1 new weapon)
+        if (currentGunIndex < maxGunIndex)
+        {
+            currentGun = currentGunIndex;
+        }
+
+        activeGun = allGuns[currentGun];
+        activeGun.gameObject.SetActive(true);
+
+        UIController.instance.ammoText.text = "AMMO: " + activeGun.currentAmmo;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("NextLevel"))
+        {
+            SaveGunData();
+            GameManager.instance.LoadNextScene();
+        }
+    }
+    
+    public void SaveGunData()//save current ammo and unlock new weapon
+    {
+        PlayerPrefs.SetInt("maxGunIndex", maxGunIndex + 1);//unlock new gun
+        for(int i=0;i<allGuns.Count; i++)//save all ammo
+        {
+            PlayerPrefs.SetInt("Gun_"+i+"_Ammo", allGuns[i].currentAmmo);
         }
     }
 }
